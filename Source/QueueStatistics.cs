abbf3d4396f02009515b0048e4465c6bc54166d0
Mikhail Shilkov <mshilkov@astrata.eu>
2017-02-28 14:56:46 +0100

    Specify Subscription ID explicitly

--- Source/QueueStatistics.cs
+++ Source/QueueStatistics.cs
@@ -7,12 +7,14 @@
     public class QueueStatistics : IDisposable
     {
         private readonly AzureManagementClient client;
+        private readonly string subscriptionId;
         private readonly string serviceBusNamespace;
         private readonly string queueName;
 
-        public QueueStatistics(X509Certificate2 certificate, string serviceBusNamespace, string queueName)
+        public QueueStatistics(X509Certificate2 certificate, string subscriptionId, string serviceBusNamespace, string queueName)
         {
             this.client = new AzureManagementClient(certificate);
+            this.subscriptionId = subscriptionId;
             this.serviceBusNamespace = serviceBusNamespace;
             this.queueName = queueName;
         }
@@ -20,7 +22,7 @@
         public DataPoint[] GetMetricSince(string metric, DateTime utcTime)
         {
             var time = utcTime.ToString("yyyy-MM-ddTHH:mm:ss") + "Z";
-            var url = $"https://management.core.windows.net/51483f81-2dd8-4fd7-9706-3fe8e873d898/services/servicebus/namespaces/{this.serviceBusNamespace}/queues/{this.queueName}/Metrics/{metric}/Rollups/PT5M/Values?$filter=Timestamp%20ge%20datetime'{time}'";
+            var url = $"https://management.core.windows.net/{this.subscriptionId}/services/servicebus/namespaces/{this.serviceBusNamespace}/queues/{this.queueName}/Metrics/{metric}/Rollups/PT5M/Values?$filter=Timestamp%20ge%20datetime'{time}'";
             var result = client.DownloadString(url);
             return JsonConvert.DeserializeObject<DataPoint[]>(result);
         }
